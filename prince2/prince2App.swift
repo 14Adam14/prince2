79fd04088fef0346c8575c748e9b46c782158388
Adam Bolotov <adam.bolotov@gmail.com>
2022-04-09 20:28:41 +0200

    Initial Commit

---
+++ prince2/prince2App.swift
@@ -0,0 +1,17 @@
+//
+//  prince2App.swift
+//  prince2
+//
+//  Created by user219374 on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct prince2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
